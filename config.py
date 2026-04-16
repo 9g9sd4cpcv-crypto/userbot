ca90c1982de6ba143924bba7836ad86a51cbef4f
9g9sd4cpcv-crypto <9g9sd4cpcv@privaterelay.appleid.com>
2026-04-16 13:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
-MONGO_URL = os.getenv("MONGO_URL")
-OPENAI_KEY = os.getenv("OPENAI_KEY")
+MONGO_URL = os.getenv("MONGO_URL") 
